2e56ec8ea4b04d7c76c41bece602a424608e5d84
Marc Zbyszynski <mzbyszynski@users.noreply.github.com>
2025-01-20 13:23:05 -0500

    Initial canvas setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { PlayerId } from "rune-sdk"
 
 import selectSoundAudio from "./assets/select.wav"
@@ -6,9 +6,26 @@ import { GameState } from "./logic.ts"
 
 const selectSound = new Audio(selectSoundAudio)
 
+const draw = (
+  ctx: CanvasRenderingContext2D | null,
+  canvas: HTMLCanvasElement
+) => {
+  if (ctx) {
+    console.log("Drawing")
+    ctx.fillStyle = "blue"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "#FF0000"
+    ctx.beginPath()
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+}
+
 function App() {
   const [game, setGame] = useState<GameState>()
   const [yourPlayerId, setYourPlayerId] = useState<PlayerId | undefined>()
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [canvasContext, setCanvasContext] = useState<CanvasRenderingContext2D>()
 
   useEffect(() => {
     Rune.initClient({
@@ -21,6 +38,15 @@ function App() {
     })
   }, [])
 
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    const context = canvas.getContext("2d")
+
+    //Our draw come here
+    draw(context, canvas)
+  }, [game])
+
   if (!game) {
     // Rune only shows your game after an onChange() so no need for loading screen
     return
@@ -28,61 +54,7 @@ function App() {
 
   const { winCombo, cells, lastMovePlayerId, playerIds, freeCells } = game
 
-  return (
-    <>
-      <div id="board" className={!lastMovePlayerId ? "initial" : ""}>
-        {cells.map((cell, cellIndex) => {
-          const cellValue = cells[cellIndex]
-
-          return (
-            <button
-              key={cellIndex}
-              onClick={() => Rune.actions.claimCell(cellIndex)}
-              data-player={(cellValue !== null
-                ? playerIds.indexOf(cellValue)
-                : -1
-              ).toString()}
-              data-dim={String(
-                (winCombo && !winCombo.includes(cellIndex)) ||
-                  (!freeCells && !winCombo)
-              )}
-              {...(cells[cellIndex] ||
-              lastMovePlayerId === yourPlayerId ||
-              winCombo
-                ? { "data-disabled": "" }
-                : {})}
-            />
-          )
-        })}
-      </div>
-      <ul id="playersSection">
-        {playerIds.map((playerId, index) => {
-          const player = Rune.getPlayerInfo(playerId)
-
-          return (
-            <li
-              key={playerId}
-              data-player={index.toString()}
-              data-your-turn={String(
-                playerIds[index] !== lastMovePlayerId && !winCombo && freeCells
-              )}
-            >
-              <img src={player.avatarUrl} />
-              <span>
-                {player.displayName}
-                {player.playerId === yourPlayerId && (
-                  <span>
-                    <br />
-                    (You)
-                  </span>
-                )}
-              </span>
-            </li>
-          )
-        })}
-      </ul>
-    </>
-  )
+  return <canvas ref={canvasRef} />
 }
 
 export default App
--- src/client.tsx
+++ src/client.tsx
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./styles.css"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById("rune-world-root") as HTMLElement
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
