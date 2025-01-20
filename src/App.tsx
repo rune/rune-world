79e4556c4524f14c48a8aae08d834fdb353ba0b2
Lam Bao Nguyen <lam@rune.ai>
2025-01-20 16:58:33 -0500

    feat: initial bodies and controls

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,79 @@ import { useEffect, useRef, useState } from "react"
 import { PlayerId } from "rune-sdk"
 
 import selectSoundAudio from "./assets/select.wav"
-import { GameState } from "./logic.ts"
+import { GameState, SPACE_HEIGHT, SPACE_WIDTH } from "./logic.ts"
+import { physics } from "propel-js"
+import nipplejs, { Joystick, JoystickManager } from "nipplejs"
 
 const selectSound = new Audio(selectSoundAudio)
 
 const draw = (
   ctx: CanvasRenderingContext2D | null,
-  canvas: HTMLCanvasElement
+  canvas: HTMLCanvasElement,
+  gameState: GameState,
 ) => {
+  // requestAnimationFrame(draw)
+  const scale = Math.min(
+    window.innerWidth / SPACE_WIDTH,
+    window.innerHeight / SPACE_HEIGHT
+  )
   if (ctx) {
-    console.log("Drawing")
-    ctx.fillStyle = "blue"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    ctx.fillStyle = "#FF0000"
-    ctx.beginPath()
-    ctx.arc(50, 100, 20, 0, 2 * Math.PI)
-    ctx.fill()
+    // console.log("Drawing", gameState.playerIds)
+
+    const world = gameState.world
+    ctx.reset()
+    ctx.resetTransform();
+
+    // const collisions = physics.worldStep(60, world);
+    
+    for (const body of physics.allBodies(world)) {
+
+      // console.log("center:", body.center)
+
+      const shape = body.shapes[0]
+      if (shape.type === physics.ShapeType.CIRCLE) {
+        // const offsetx =
+        //   ((body.center.x - SPACE_WIDTH / 2) / (SPACE_WIDTH / 2)) * (4 / scale)
+        // const offsety =
+        //   ((body.center.y - SPACE_HEIGHT / 2) / (SPACE_HEIGHT / 2)) * (4 / scale)
+        // ctx.fillStyle = "yellow"
+        // ctx.beginPath()
+        // ctx.arc(
+        //   body.center.x + offsetx * scale,
+        //   body.center.y + offsety * scale,
+        //   shape.bounds,
+        //   0,
+        //   Math.PI * 2
+        // )
+        // ctx.fill()
+
+        ctx.save();
+        ctx.translate(shape.center.x, shape.center.y);
+        ctx.rotate(body.angle);
+
+        ctx.beginPath();
+        ctx.arc(0, 0, shape.bounds, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, shape.bounds);
+        ctx.stroke();
+
+        ctx.restore();
+      }
+
+      if (!body.static) {
+        const dynamic = body as physics.DynamicRigidBody;
+        ctx.fillStyle = "blue";
+        ctx.beginPath();
+        ctx.arc(dynamic.centerOfPhysics.x, dynamic.centerOfPhysics.y, 2, 0, Math.PI * 2);
+        ctx.fill();
+      }
+      ctx.fillStyle = "red";
+      ctx.beginPath();
+      ctx.arc(body.center.x, body.center.y, 2, 0, Math.PI * 2);
+      ctx.fill();
+    }
   }
 }
 
@@ -32,31 +89,60 @@ const getCanvasDimensions = () => {
   }
 }
 
+type JoystickState = {
+  x: number
+  y: number
+}
+
 function App() {
+  const joystickStateRef = useRef<JoystickState>({x: 0, y: 0})
   const [game, setGame] = useState<GameState>()
   const [yourPlayerId, setYourPlayerId] = useState<PlayerId | undefined>()
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const {height, width} = getCanvasDimensions()
   const [canvasContext, setCanvasContext] = useState<CanvasRenderingContext2D>()
+  const controllerDivRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     Rune.initClient({
-      onChange: ({ game, action, yourPlayerId }) => {
+      onChange: ({ game, yourPlayerId }) => {
         setGame(game)
         setYourPlayerId(yourPlayerId)
-
-        if (action && action.name === "claimCell") selectSound.play()
       },
     })
   }, [])
 
   useEffect(() => {
+    if (controllerDivRef.current) {
+      const joystick = nipplejs.create({
+        mode: "static",
+        zone: controllerDivRef.current,
+        position: { left: "80px", bottom: "80px" },
+        threshold: 0.2,
+        color: "blue",
+      })
+
+      joystick.on("move", (event, joystick) => {
+        const newControls = joystick.vector
+        
+        // clamp inputs to stop stutter
+        newControls.x = Math.floor(newControls.x * 30) / 30
+        newControls.y = Math.floor(newControls.y * 30) / 30
+
+        Rune.actions.move(newControls)
+      })
+
+      return () => joystick.destroy()
+    }
+  }, [controllerDivRef.current])
+
+  useEffect(() => {
     const canvas = canvasRef.current
-    if (!canvas) return
+    if (!canvas || !game) return
     const context = canvas.getContext("2d")
 
     //Our draw come here
-    draw(context, canvas)
+    draw(context, canvas, game, joystickStateRef.current)
   }, [game])
 
   if (!game) {
@@ -66,7 +152,12 @@ function App() {
 
   const { winCombo, cells, lastMovePlayerId, playerIds, freeCells } = game
 
-  return <canvas width={width} height={height} ref={canvasRef} />
+  return (
+    <>
+      <canvas width={width} height={height} ref={canvasRef} />
+      <div id="controller" ref={controllerDivRef} />
+    </>
+  )
 }
 
 export default App
--- src/logic.ts
+++ src/logic.ts
@@ -1,80 +1,87 @@
 import type { PlayerId, RuneClient } from "rune-sdk"
+import { physics } from "propel-js"
 
 export type Cells = (PlayerId | null)[]
+
 export interface GameState {
-  cells: Cells
-  winCombo: number[] | null
-  lastMovePlayerId: PlayerId | null
   playerIds: PlayerId[]
-  freeCells?: boolean
+  world: physics.World
+  playerBodies: Record<PlayerId, number>
 }
 
 type GameActions = {
-  claimCell: (cellIndex: number) => void
+  move: (controls: { x: number, y: number}) => void
+}
+
+type Controls = {
+  x: number
+  y: number
 }
 
 declare global {
   const Rune: RuneClient<GameState, GameActions>
 }
 
-function findWinningCombo(cells: Cells) {
-  return (
-    [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ].find((combo) =>
-      combo.every((i) => cells[i] && cells[i] === cells[combo[0]])
-    ) || null
-  )
-}
+export const SPACE_WIDTH = 250
+export const SPACE_HEIGHT = 140
+export const SHIP_SIZE = 10
 
 Rune.initLogic({
-  minPlayers: 2,
-  maxPlayers: 2,
-  setup: (allPlayerIds) => ({
-    cells: new Array(9).fill(null),
-    winCombo: null,
-    lastMovePlayerId: null,
-    playerIds: allPlayerIds,
-  }),
+  minPlayers: 1,
+  maxPlayers: 6,
+  updatesPerSecond: 30,
+  reactive: false,
+  update: ({ game }) => {
+    const collisions = physics.worldStep(60, game.world)
+  },
+  setup: (allPlayerIds) => {
+    const world = physics.createWorld({ x: 0, y: 0 }, 20)
+    const playerBodies: Record<PlayerId, number> = {}
+    allPlayerIds.forEach((playerId, i) => {
+      const player: physics.DynamicRigidBody = physics.createCircle(
+        world,
+        { x: (SPACE_WIDTH / 6) * (i + 1), y: SPACE_HEIGHT / 2 },
+        SHIP_SIZE,
+        1,
+        1,
+        1
+      ) as physics.DynamicRigidBody
+
+      player.data = {
+        col: 'orange',
+        num: 0,
+        ox: (SPACE_WIDTH / 6),
+        oy: SPACE_HEIGHT / 2,
+      }
+
+      playerBodies[playerId] = player.id
+
+      physics.addBody(world, player)
+
+      player.velocity.y = 25
+
+      // physics.applyVelocity(player, {
+      //   x: 200,
+      //   y: 200,
+      // })
+    })
+
+    const gameState: GameState = {
+      world,
+      playerIds: allPlayerIds,
+      playerBodies,
+    }
+
+    return gameState
+},
   actions: {
-    claimCell: (cellIndex, { game, playerId, allPlayerIds }) => {
-      if (
-        game.cells[cellIndex] !== null ||
-        playerId === game.lastMovePlayerId
-      ) {
-        throw Rune.invalidAction()
-      }
-
-      game.cells[cellIndex] = playerId
-      game.lastMovePlayerId = playerId
-      game.winCombo = findWinningCombo(game.cells)
-
-      if (game.winCombo) {
-        const [player1, player2] = allPlayerIds
-
-        Rune.gameOver({
-          players: {
-            [player1]: game.lastMovePlayerId === player1 ? "WON" : "LOST",
-            [player2]: game.lastMovePlayerId === player2 ? "WON" : "LOST",
-          },
-        })
-      }
-
-      game.freeCells = game.cells.findIndex((cell) => cell === null) !== -1
-
-      if (!game.freeCells) {
-        Rune.gameOver({
-          players: {
-            [game.playerIds[0]]: "LOST",
-            [game.playerIds[1]]: "LOST",
-          },
+    move: (controls: Controls, { game, playerId }) => {
+      const playerBodies = game.playerBodies
+      const playerBody = game.world.dynamicBodies.find((b) => b.id === playerBodies[playerId])
+      if (playerBody) {
+        physics.applyVelocity(playerBody, {
+          x: controls.x,
+          y: controls.y,
         })
       }
     },
