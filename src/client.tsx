7ecbeb6889008ea954cf78e759266a0f6737c43b
Marc Zbyszynski <mzbyszynski@users.noreply.github.com>
2025-01-22 12:05:39 -0500

    fixing joystick

--- src/client.tsx
+++ src/client.tsx
@@ -2,7 +2,11 @@ import { renderJoystick } from "./joystick.ts"
 import type { Controls } from "./logic.ts"
 import { renderGame } from "./render.ts"
 
-const onJoystickMove = (controls: Controls) => Rune.actions.move(controls)
+const onJoystickMove = (controls: Controls) => {
+  Rune.actions.move(controls)
+}
+
+let firstRender = true
 
 const startUI = () => {
   Rune.initClient({
@@ -14,14 +18,21 @@ const startUI = () => {
         yourPlayerId
       ) {
         renderGame({ game, playerId: yourPlayerId, rootDiv: runeWorldRootDiv })
+
+        if (firstRender) {
+          renderJoystick({ onMove: onJoystickMove })
+          // added this to clear out invalid initial states that
+          // can happen on load
+          setTimeout(() => {
+            renderJoystick({
+              onMove: onJoystickMove,
+            })
+          }, 1000)
+          firstRender = false
+        }
       }
     },
   })
-
-  const joystickDiv = document.getElementById("joystick")
-  if (joystickDiv && joystickDiv instanceof HTMLDivElement) {
-    renderJoystick({ zone: joystickDiv, onMove: onJoystickMove })
-  }
 }
 
 startUI()
--- src/joystick.ts
+++ src/joystick.ts
@@ -1,5 +1,5 @@
 import { type Controls } from "./logic"
-import nipplejs from "nipplejs"
+import nipplejs, { Joystick, JoystickManager } from "nipplejs"
 
 const lastMoveOldEnough = (date: Date) => {
   return new Date().getTime() - date.getTime() > 100
@@ -7,20 +7,30 @@ const lastMoveOldEnough = (date: Date) => {
 
 let lastMoveTime: Date | undefined = undefined
 
+let joystick: JoystickManager | undefined = undefined
+let controller: Joystick | undefined = undefined
+
 export const renderJoystick = ({
   onMove,
-  zone,
 }: {
   onMove: (controls: Controls) => void
-  zone: HTMLDivElement
 }) => {
-  const joystick = nipplejs.create({
+  if (joystick) {
+    joystick.destroy()
+  }
+  if (controller) {
+    controller.remove()
+  }
+
+  joystick = nipplejs.create({
     mode: "static",
-    zone,
     position: { left: "80px", bottom: "80px" },
     threshold: 0.2,
     color: "#FAEF98",
+    dynamicPage: true,
   })
+
+  controller = joystick.get(joystick.ids[0])
 
   joystick.on("move", (event, joystick) => {
     if (!lastMoveTime || lastMoveOldEnough(lastMoveTime)) {
